77f87847d7d495423eaca0c4c0ccdeb3517a03e4
christy <vennesach@gmail.com>
2023-03-11 18:46:59 +0800

    1.新增樂透號碼 2.增加字串串接 3.測試完畢

--- 1.py
+++ 1.py
@@ -1,4 +1,9 @@
 import random
 
+# 樂透號碼開獎：1 5 10 35 50
+# 特別號：49
+
 numbers = random.sample(range(1, 50), 6)
-print(numbers)
+print('樂透號碼開獎: '+' '.join(map(str, sorted(numbers))))
+
+print(f'特別號：{random.randint(1, 50)}')
