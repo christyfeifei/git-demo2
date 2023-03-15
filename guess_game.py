d09ec5b733f5ad97449a1d860c697cf69a3378e0
christy <vennesach@gmail.com>
2023-03-15 16:14:22 +0800

    新增區間提示，測試ok

--- guess_game.py
+++ guess_game.py
@@ -5,8 +5,9 @@ import random
 # 3.提示區間
 
 x = random.randint(1, 50)
-print(x)
+
 win = False
+start, end = 1, 50
 for i in range(5):
     y = eval(input(f'[{i+1}/5]請猜一個數字(1~50之間):'))
     if x == y:
@@ -14,9 +15,15 @@ for i in range(5):
         break
 
     if x > y:
-        print('猜大一點')
+        # 如果y大於原本最小值再進行替換
+        if y > start:
+            start = y
+        print(f'猜大一點({start}~{end})')
     else:
-        print('猜小一點')
+        # 如果y小於原本最高值再進行替換
+        if y < end:
+            end = y
+        print(f'猜小一點({start}~{end})')
 
 
 if win:
