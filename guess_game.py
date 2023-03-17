cbe1c011bbdaabef233084ac72414851f716ee28
christy <vennesach@gmail.com>
2023-03-17 13:25:37 +0800

    修改程式碼

--- guess_game.py
+++ guess_game.py
@@ -6,30 +6,18 @@ import random
 
 x = random.randint(1, 50)
 
-print(x)
 win = False
-start, end = 1, 50
-
+start, end = 1, 100
 
 for i in range(5):
-    y = eval(input(f'[{i+1}/5]請猜一個數字({start}~{end}):'))
+    y = eval(input(f'[{i+1}/5]請猜一個數字({start}~{end}之間):'))
     if x == y:
         win = True
         break
 
     if x > y:
-        # 如果y大於原本最小值再進行替換
-        if y > start:
-            start = y
         print('猜大一點')
     else:
-        # 如果y小於原本最大值再進行替換
-        if y < end:
-            end = y
-        print(f'猜低一點')
+        print('猜小一點')
 
-
-if win:
-    print(f'恭禧過關!總共猜:{i+1}次')
-else:
-    print(f'遊戲失敗!答案為:{x}')
+print(f'遊戲結束!答案為:{x}')
